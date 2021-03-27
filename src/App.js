c0caaf3e0e107dd74353c50ff8441a94818e66b2
Mark Lusala Mudoga <lusalamark63@gmail.com>
2021-03-27 16:01:16 +0300

    header section complete

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import "./App.css";
 import Header from "./Header";
 
 function App() {
-  return <div className="app">
-    <Header />
-    {/*App Body */}
-    {/*Side bar */}
-    {/*Feed */}
-    {/*Widgets */}    
-  </div>;
+  return (
+    <div className="app">
+      <Header />
+      <div className="app_body">
+        <Sidebar />
+        {/*Side bar */}
+        {/*Feed */}
+        {/*Widgets */}
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -1,21 +1,31 @@
 import SearchIcon from "@material-ui/icons/Search";
 import React from "react";
 import "./Header.css";
+import HeaderOption from "./HeaderOption";
+import HomeIcon from "@material-ui/icons/Home";
+import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+import ChatIcon from "@material-ui/icons/Chat";
+import NotificationsIcon from "@material-ui/icons/Notifications";
 
 function Header() {
   return (
     <div className="header">
-      <div className="heade__left">
+      <div className="header__left">
         {/*<img src="inkedin/src/assets/logo.png" alt="LINKEDIN" />*/}
         <div className="header__search">
-          {/*SearchIcon */}
           <SearchIcon />
           <input type="text" />
         </div>
       </div>
 
       <div className="header__right">
-           
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+        <HeaderOption avatar = "" title='Me' />
       </div>
     </div>
   );
