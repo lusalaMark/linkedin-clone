c1ece18665489740044a89c5e0d79c4e145c203f
Mark Lusala Mudoga <lusalamark63@gmail.com>
2021-03-27 14:20:36 +0300

    Finisht with the right hand side of the website

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,12 @@
 import SearchIcon from "@material-ui/icons/Search";
 import React from "react";
+import "./Header.css";
 
 function Header() {
   return (
     <div className="header">
       <div className="heade__left">
-        <img src="" alt="" />
-
+        {/*<img src="inkedin/src/assets/logo.png" alt="LINKEDIN" />*/}
         <div className="header__search">
           {/*SearchIcon */}
           <SearchIcon />
@@ -14,7 +14,9 @@ function Header() {
         </div>
       </div>
 
-      <div className="header__right"></div>
+      <div className="header__right">
+           
+      </div>
     </div>
   );
 }
