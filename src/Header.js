8a8eb7fb764b640b9309cb1e2fcf1621021363c3
Mark Lusala Mudoga <lusalamark63@gmail.com>
2021-03-29 01:16:23 +0300

    Registering push

--- src/Header.js
+++ src/Header.js
@@ -7,8 +7,16 @@ import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
 import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
 import ChatIcon from "@material-ui/icons/Chat";
 import NotificationsIcon from "@material-ui/icons/Notifications";
+import { useDispatch } from "react-redux";
+import { auth } from "./firebase";
 
 function Header() {
+  const dispatch = useDispatch();
+
+  const logoutOfApp = () => {
+    dispatch(logout());
+    auth.signOut();
+  };
   return (
     <div className="header">
       <div className="header__left">
@@ -25,7 +33,7 @@ function Header() {
         <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
         <HeaderOption Icon={ChatIcon} title="Messaging" />
         <HeaderOption Icon={NotificationsIcon} title="Notifications" />
-        <HeaderOption avatar="" title="Me" />
+        <HeaderOption avatar="" title="Me" onClick={logoutOfApp} />
       </div>
     </div>
   );
