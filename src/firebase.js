a03b738f9c16e78b0bf0ae79ffca34d1ec1967c1
Mark Lusala Mudoga <lusalamark63@gmail.com>
2021-03-29 08:35:44 +0300

    firebase configuration

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,20 @@
 import firebase from "firebase";
 
-const firebaseConfig = {};
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCST13R70oEgSD0-PrhInzaNH_RW3MjDNI",
+  authDomain: "linkedin-clone-6229f.firebaseapp.com",
+  projectId: "linkedin-clone-6229f",
+  storageBucket: "linkedin-clone-6229f.appspot.com",
+  messagingSenderId: "26612393048",
+  appId: "1:26612393048:web:918e902aef1d097a38d62d",
+  measurementId: "G-WT7RTNXST8",
+};
 
-const firebase = firebase.initializeApp(firebaseConfig);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
 const db = firebaseApp.firestore();
+
 const auth = firebase.auth();
 
 export { db, auth };
